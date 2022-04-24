b9fc08fd3afa0a1358a63d2c501ad0cc130b3a6f
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2022-04-24 16:52:53 +0530

    Updated

---
+++ Weekly/weekly-contest-272/C.java
@@ -0,0 +1,16 @@
+class Solution {
+    // If i and i-1 th elements form a valid descent, then ith can extend all the descents that i-1 has.
+    // DP STATE : dp[i] = No. of descents that end at ith
+    // DP TRANS : dp[i] = 1 + dp[i-1]
+    public long getDescentPeriods(int[] prices) {
+        int n = prices.length;
+        long dp[] = new long[n];
+        Arrays.fill(dp, 1L);
+        for(int i = 1; i<n; i++)
+        {
+            if( prices[i]+1 == prices[i-1])
+                dp[i]+=dp[i-1];
+        }
+        return Arrays.stream(dp).sum();
+    }
+}
